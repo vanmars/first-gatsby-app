9ad6e6f6a0301cbb11d77b14a504d436833c8681
Vanessa Stewart <vstewart1213@gmail.com>
2021-05-11 15:30:43 -0700

    Setup Gatsy project and add initial pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return ( 
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis non autem beatae quia expedita, sint blanditiis cumque nesciunt voluptas obcaecati assumenda! Repellendus sequi obcaecati nostrum impedit aspernatur illum dicta voluptatum.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis non autem beatae quia expedita, sint blanditiis cumque nesciunt voluptas obcaecati assumenda! Repellendus sequi obcaecati nostrum impedit aspernatur illum dicta voluptatum.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis non autem beatae quia expedita, sint blanditiis cumque nesciunt voluptas obcaecati assumenda! Repellendus sequi obcaecati nostrum impedit aspernatur illum dicta voluptatum.</p>
+    </div>
+   );
+}
+ 
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h2>Develop & Design</h2>
+        <p>Web developer based in the Pacific Northwest.</p>
+      </div>
+    </section>
+  )
 }
